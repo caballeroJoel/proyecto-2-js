b644d809f474ec95c0b35965671a33f8fd09c90a
Joel Caballero Lucia <caballeroluciajoel@fpllefia.com>
2026-04-16 18:25:12 +0200

    casi todo

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
-import { destinacionsDestacades, ofertes, hotelsPopulars, searchResultsData, productDetailData, checkoutData, myTripsData } from "./data.js";
+let supabaseUrl = "https://zorudgbkbgddrhyvowyq.supabase.co";
+let supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvcnVkZ2JrYmdkZHJoeXZvd3lxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzNDEwNzYsImV4cCI6MjA5MTkxNzA3Nn0.hTj6ZFQmxptpcn8rJzFHSMd7DttrNwS0ZJYGsUpxN2Y"; 
+
+let client = supabase.createClient(supabaseUrl, supabaseAnonKey);
 
 const cls = {
 
@@ -66,13 +69,17 @@ function render() {
 }
 
 /****************** Home ******************/
-function vacacionesDestacadas() {
-    let html = '';
+async function vacacionesDestacadas() {
 
-    destinacionsDestacades.forEach(dest => {
+    let resultat = await client.from("destinacions_destacades").select("*");
+    let destinations = resultat.data || [];
+    
+    let html = "";
+
+    destinations.forEach(dest => {
         html+= `
             <div>
-                <img src="../img/cont/${dest.imatgeUrl}.jpg" alt="${dest.id}">
+                <img src="../img/cont/${dest.imatge_url}.jpg" alt="${dest.id}">
                 <p>${dest.nom}</p>
                 <span>${dest.propietatsCount} properties</span>
             </div>
@@ -81,14 +88,18 @@ function vacacionesDestacadas() {
 
     cls.vacationOffers.innerHTML = html;
 }
-function ofertas() {
+async function ofertas() {
+
+    let resultat = await client.from("ofertes_destacades").select("*");
+    let ofertes = resultat.data || [];
+
     let html = '';
 
     ofertes.forEach(of => {
         html+=`
             <div class="inspiration-item">
                 <div>
-                    <img src="./img/inspi/${of.imatgeUrl}.jpg">
+                    <img src="./img/inspi/${of.imatge_url}.jpg">
                 </div>
                 <div class="cont">
                     <p>${of.titol}</p>
@@ -100,13 +111,16 @@ function ofertas() {
 
     cls.inspirationItems.innerHTML = html;
 }
-function popularHotel() {
+async function popularHotel() {
     let html='';
 
-    hotelsPopulars.forEach(pop => {
+    let resultat = await client.from("hotels_populars").select("*");
+    let hotels = resultat.data || [];
+
+    hotels.forEach(pop => {
         html += `
             <div class="hotel-item" id="${pop.id}">
-                <img src="../img/hoteles/${pop.imatgeUrl}.jpg" alt="">
+                <img src="../img/hoteles/${pop.imatge_url}.jpg" alt="">
                 <p>${pop.nom}</p>
                 <span>${pop.propietatsCount} properties</span>
             </div>
@@ -117,71 +131,94 @@ function popularHotel() {
 }
 
 /****************** Search ******************/
-function searchFilters() {
+async function searchFilters() {
     let html = '';
     let html1 = '';
-    
-    cls.querySearch.querySelector("#destination").value = searchResultsData.query.where;
-    cls.querySearch.querySelector("#checkin").value = searchResultsData.query.checkin;
-    cls.querySearch.querySelector("#checkout").value = searchResultsData.query.checkout;
-    cls.querySearch.querySelector("#guests").value = searchResultsData.query.guests;
 
-    cls.searchResults.innerHTML = `${searchResultsData.query.where} : ${searchResultsData.totalResults} search results found`;
+    let { data, error } = await client
+        .from("search_results_data")
+        .select("*")
+        .single();
 
+    if (error) {
+        console.error(error);
+        return;
+    }
 
-    searchResultsData.filters.budgetRanges.forEach(filt => {
-        html+= `
+    const query = data.query;
+    const filters = data.filters;
+
+    cls.querySearch.querySelector("#destination").value = query.where;
+    cls.querySearch.querySelector("#checkin").value = query.checkin;
+    cls.querySearch.querySelector("#checkout").value = query.checkout;
+    cls.querySearch.querySelector("#guests").value = query.guests;
+
+    cls.searchResults.innerHTML = `${query.where} : ${data.total_results} search results found`;
+
+    filters.budgetRanges.forEach(filt => {
+        html += `
             <input type="checkbox" name="price" id="${filt.min}-${filt.max}">
             <span>$ ${filt.min} - $ ${filt.max}</span>
         `;
-        html1 += `  
-            <span>${filt.count}</span>
-        `;
+        html1 += `<span>${filt.count}</span>`;
     });
+
     cls.filtertBudget.innerHTML = html;
     cls.filtertBudgetCant.innerHTML = html1;
-    html="";
-    html1="";
-    
-    searchResultsData.filters.activities.forEach(filt => {
+
+    html = "";
+    html1 = "";
+
+    filters.activities.forEach(filt => {
         html += `
-        <input type="checkbox" name="activities" id="${filt.id}">
-        <span>${filt.label}</span>
+            <input type="checkbox" name="activities" id="${filt.id}">
+            <span>${filt.label}</span>
         `;
-        html1 += `
-        <span>${filt.count}</span>
-        `;
+        html1 += `<span>${filt.count}</span>`;
     });
+
     cls.filterActivitis.innerHTML = html;
     cls.filterActivitisCant.innerHTML = html1;
-    html="";
-    html1="";
-    
-    searchResultsData.filters.popularFilters.forEach(filt => {
+
+    html = "";
+    html1 = "";
+
+    filters.popularFilters.forEach(filt => {
         html += `
             <input type="checkbox" name="popular" id="${filt.id}">
             <span>${filt.label}</span>
         `;
-        html1 += `
-            <span>${filt.count}</span>
-        `;
+        html1 += `<span>${filt.count}</span>`;
     });
 
     cls.filterPopular.innerHTML = html;
     cls.filterPopularCant.innerHTML = html1;
-    html="";
-    html1="";
-
 }
-function searchResult() {
+async function searchResult() {
 
-    let html="";
-    let html1="";
+    let html = "";
+    let html1 = "";
 
-    searchResultsData.results.forEach(sch => {
+    let { data, error } = await client
+        .from("search_results_data")
+        .select("results")
+        .single();
 
-        html1 = sch.badge ? `<p class="${sch.badgeCol}">${sch.badge}</p>` : "";
-       
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    const results = typeof data.results === "string"
+        ? JSON.parse(data.results)
+        : data.results;
+
+    results.forEach(sch => {
+
+        html1 = sch.badge 
+            ? `<p class="${sch.badgeCol}">${sch.badge}</p>` 
+            : "";
+
         html += `
             <div class="container">
                 <div>
@@ -223,16 +260,26 @@ function searchResult() {
         `;
     });
 
-    console.log(html);
-
     listResult.innerHTML = html;
-
 }
 
 /***************** Prodcut *****************/
-function productDetail() {
+async function productDetail() {
     let htmlImg = '', htmlName='', hmtlOverview='', htmlFacilities = '';
     let htmlArea = '', htmlRooms = '';
+
+    let { data, error } = await client
+        .from("product_detail_data")
+        .select("*")
+        .single();
+
+    if (error) {
+        console.error(error);
+        return;
+    }
+    const productDetailData = data;
+
+    console.log(productDetailData);
 
     htmlImg += `
         <div>
@@ -273,7 +320,7 @@ function productDetail() {
     `;
     cls.nameProd.innerHTML = htmlName;
 
-    productDetailData.topFacilities.forEach(fac => {
+    productDetailData.top_facilities.forEach(fac => {
         htmlFacilities += `
             <div>
                 <img src="./img/icons_prod/${fac.id}.png" alt="">
@@ -285,7 +332,7 @@ function productDetail() {
     hmtlOverview = `
         <p>Overview</p>
         <div class="overview-span">
-            <span>${productDetailData.hotel.overviewText}</span>
+            <span>${productDetailData.hotel.overview_text}</span>
         </div>
         <hr>
         <p>Top facilities</p>
@@ -296,7 +343,7 @@ function productDetail() {
     cls.overviewProduct.innerHTML = hmtlOverview;
 
     let html="";
-    productDetailData.exploreArea.forEach(area => {
+    productDetailData.explore_area.forEach(area => {
         html += `
             <div>
                 <div>
@@ -377,7 +424,7 @@ function productDetail() {
                             <img src="./img/avion_wh.png" alt="">
                             <span>my Dream Place</span>
                         </div>
-                        <p>${productDetailData.promoCard.title}</p>
+                        <p>${productDetailData.promo_card.title}</p>
                     </div>
                     <div>
                         <img class="person" src="./img/products/person.png" alt="">
@@ -392,7 +439,19 @@ function productDetail() {
 }
 
 /****************** Reserv ******************/
-function room() {
+async function room() {
+
+    let { data, error } = await client
+        .from("checkout_data")
+        .select("*")
+        .single();
+
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    const checkoutData = data;
 
     let html = `
         <img src="./img/icons_prod/person_shield.png" alt="">
@@ -403,10 +462,22 @@ function room() {
 
     cls.titleRoom.innerHTML = html;
 }
-function reservation() {
+async function reservation() {
     let html = '', html2 = '';
 
-    checkoutData.priceDetails.items.forEach(item => {
+    let { data, error } = await client
+        .from("checkout_data")
+        .select("*")
+        .single();
+
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    const checkoutData = data;
+
+    checkoutData.price_details.items.forEach(item => {
         html2 += `
             <div>
                 <span>$ ${item.description}</span>
@@ -418,11 +489,11 @@ function reservation() {
     html = `
         <div class="resumen">
             <div class="img">
-                <img src="./img/products/${checkoutData.summaryCard.hotelId}_2.png" alt="">
+                <img src="./img/products/${checkoutData.summary_card.hotelId}_2.png" alt="">
             </div>
             <div class="resm">
                 <div>
-                    <p>${checkoutData.summaryCard.hotelName}</p>
+                    <p>${checkoutData.summary_card.hotelName}</p>
                     <div class="stars-rating">
                         <div>
                             <img src="./img/star.png" alt="">
@@ -431,14 +502,14 @@ function reservation() {
                             <img src="./img/star.png" alt="">
                             <img src="./img/star.png" alt="">
                         </div>
-                        <p>${checkoutData.summaryCard.rating} (${checkoutData.summaryCard.reviewCount} Reviews)</p>
+                        <p>${checkoutData.summary_card.rating} (${checkoutData.summary_card.reviewCount} Reviews)</p>
                     </div>
                 </div>
                 <div class="prop">
-                    <span class="marc">${checkoutData.summaryCard.policy}</span>
-                    <span>Check in: ${checkoutData.summaryCard.checkIn}</span>
-                    <span>Check out: ${checkoutData.summaryCard.checkOut}</span>
-                    <span>${checkoutData.summaryCard.stayNights} night stay</span>
+                    <span class="marc">${checkoutData.summary_card.policy}</span>
+                    <span>Check in: ${checkoutData.summary_card.checkIn}</span>
+                    <span>Check out: ${checkoutData.summary_card.checkOut}</span>
+                    <span>${checkoutData.summary_card.stayNights} night stay</span>
                 </div>
             </div>
         </div>
@@ -455,7 +526,7 @@ function reservation() {
                 <div class="total">
                     <div class="precio_total">
                         <p>Total</p>
-                        <p class="precio">${checkoutData.priceDetails.currency}${checkoutData.priceDetails.total}</p>
+                        <p class="precio">${checkoutData.price_details.currency}${checkoutData.price_details.total}</p>
                     </div>
                     <span>Use a coupon, credit or promotional code</span>
 
@@ -474,11 +545,23 @@ function reservation() {
     cls.resumenReser.innerHTML = html;
 
 }
-function importantInfo() {
+async function importantInfo() {
 
     let html2 = '';
 
-    checkoutData.policyItems.forEach(item => {
+    let { data, error } = await client
+        .from("checkout_data")
+        .select("*")
+        .single();
+
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    const checkoutData = data;
+
+    checkoutData.policy_items.forEach(item => {
         html2+= `
             <li>${item}</li>
         `;
--- js/data.js
+++ js/data.js
@@ -1,323 +1,129 @@
-export const destinacionsDestacades = [
-  {
-    id: "australia",
-    nom: "Australia",
-    imatgeUrl: "australia",
-    propietatsCount: 2246,
-  },
-  {
-    id: "japan",
-    nom: "Japan",
-    imatgeUrl: "japon",
-    propietatsCount: 1278,
-  },
-  {
-    id: "new-zealand",
-    nom: "New Zealand",
-    imatgeUrl: "nueva-zelanda",
-    propietatsCount: 480,
-  },
-  {
-    id: "greece",
-    nom: "Greece",
-    imatgeUrl: "grecia",
-    propietatsCount: 320,
-  },
-];
+// export const productDetailData = {
+//   hotel: {
+//     id: "lakeside-motel-warefront",
+//     name: "Lakeside Motel Warefront",
+//     rating: 4.5,
+//     reviewsCount: 1200,
+//     address: "Lorem ipsum road, Tantri\n-2322, Melbourne, Australia",
+//     overviewText:
+//       "Featuring free WiFi throughout the property, Lakeside Motel Waterfront offers accommodations in Lakes Entrance, 19 mi from Bairnsdale. Free private parking is available on site.<br><br>Each room at this motel is air conditioned and comes with a flat-screen TV. You will find a kettle, toaster and a microwave in the room. Each room is fitted with a private bathroom. Guests have access to barbecue facilities and a lovely large lawn area. Metung is 6.8 mi from Lakeside Motel Waterfront, while Paynesville is 14 mi from the property.<br><br>Couples in particular like the location – they rated it 9.2 for a two-person trip.",
+//   },
+//   topFacilities: [
+//     { id: "wifi", label: "Free wifi" },
+//     { id: "ac", label: "Air Conditioning" },
+//     { id: "parking", label: "Parking available" },
+//     { id: "business", label: "Business Services" },
+//     { id: "pool", label: "Swimming pool" },
+//     { id: "top-rated", label: "Top rated in area" },
+//   ],
+//   exploreArea: [
+//     { name: "Hotel Pennsylvania", distance: "2 min drive" },
+//     { name: "Travis Bakery store house", distance: "10 min drive" },
+//     { name: "Olivia Johnson Garden", distance: "15 min drive" },
+//     { name: "Norman Opera Circus", distance: "18 min drive" },
+//     { name: "Rockdesert hotel", distance: "32 min drive" },
+//   ],
+//   availability: {
+//     checkin: "Mar 18, 2022",
+//     checkout: "March 20, 2022",
+//     guests: "2 adult, 1 room",
+//   },
+//   promoCard: {
+//     title: "20% off<br>Use Promotional<br>Coupon Code:<br><span>Orlando</span>",
+//   },
+//   rooms: [
+//     {
+//       id: "standard-twin-multiple-beds",
+//       name: "Standard twin ben, Multiple beds",
+//       price: 300,
+//       currency: "$",
+//       sleeps: 3,
+//       beds: "1 double bed and 1 twin bed",
+//     },
+//     {
+//       id: "standard-twin-queen-bed",
+//       name: "Standard twin ben, 1 Queen bed",
+//       price: 300,
+//       currency: "$",
+//       sleeps: 3,
+//       beds: "1 double bed and 1 twin bed",
+//     },
+//   ],
+// };
 
-export const ofertes = [
-  {
-    id: "sydneys-10-most-fashionable-5-star-hotels",
-    titol: "Sydney's 10 Most Fashionable 5 Star Hotels",
-    descripcio:
-      "Browse the fastest growing tourism sector in the heart of Australia tourism Capital.",
-    imatgeUrl: "sydney",
-  },
-  {
-    id: "vegan-travel",
-    titol: "Top cities for Vegan Travellers",
-    descripcio:
-      "Top sites where you do not have to worry about being a vegan. Our tourist guide is here...",
-    imatgeUrl: "vegan",
-  },
-  {
-    id: "post-covid",
-    titol: "World's top destinations during and post covid timeline",
-    descripcio:
-      "Pandemic is still here and will be here for a longer time. Here's where your next destination...",
-    imatgeUrl: "covid",
-  },
-];
+// export const checkoutData = {
+//   room: {
+//     roomLabel: "Room 1",
+//     summary: "2 adults, 1 double bed and 1 twin bed, Non-smoking",
+//   },
+//   summaryCard: {
+//     hotelId: "lakeside-motel-warefront",
+//     hotelName: "Lakeside Motel Warefront",
+//     rating: 4.5,
+//     reviewsCount: 1200,
+//     policy: "Non refundable",
+//     checkIn: "Sunday, March 18, 2022",
+//     checkOut: "Tuesday, March 20, 2022",
+//     stayNights: 2,
+//   },
+//   priceDetails: {
+//     items: [
+//       { description: "1 room X 2 nights", amount: 120.32 },
+//       { description: "Tax and service fees", amount: 8.32 },
+//     ],
+//     total: 130,
+//     currency: "$",
+//   },
+//   policyItems: [
+//     "This rate is non-refundable. If you change or cancel your booking you will not get a refund or credit to use for a future stay.",
+//     "Stay extensions will require a new reservation.",
+//     "Front desk staff will greet guests on arrival",
+//     "No refunds will be issued for late check-in or early check-out.",
+//   ],
+// };
 
-export const hotelsPopulars = [
-  {
-    id: "lakeside-motel-warefront",
-    nom: "Lakeside Motel Warefront",
-    imatgeUrl: "lakeside-motel",
-    propietatsCount: 2246,
-  },
-  {
-    id: "rocce-graham-resort",
-    nom: "Rocce Graham resort",
-    imatgeUrl: "rocce-graham",
-    propietatsCount: 1278,
-  },
-  {
-    id: "fireside-dinners",
-    nom: "Fireside Dinners",
-    imatgeUrl: "fireside-dinners",
-    propietatsCount: 480,
-  },
-  {
-    id: "oculous-inn-stay",
-    nom: "Oculous Inn Stay",
-    imatgeUrl: "oculous-inn",
-    propietatsCount: 320,
-  },
-];
-
-export const searchResultsData = {
-  query: {
-    where: "Melbourne",
-    checkin: "2022-03-18",
-    checkout: "2022-03-20",
-    guests: "2 adult, 1 room",
-  },
-  totalResults: 2582,
-  filters: {
-    budgetRanges: [
-      { min: 0, max: 200, count: 200 },
-      { min: 250, max: 500, count: 19 },
-      { min: 500, max: 1000, count: 12 },
-      { min: 1000, max: 2000, count: 12 },
-      { min: 2000, max: 8000, count: 230 },
-    ],
-    popularFilters: [
-      { id: "free-cancellation", label: "Free cancellation", count: 200 },
-      { id: "beachfront", label: "Beach front", count: 100 },
-      { id: "hot-tub", label: "Hot tub/Jacuzzi", count: 15 },
-      { id: "no-credit-card", label: "Book without credit card", count: 12 },
-      { id: "pet-friendly", label: "Pet friendly", count: 200 },
-    ],
-    activities: [
-      { id: "fishing", label: "Fishing", count: 200 },
-      { id: "hiking", label: "Hiking", count: 100 },
-      { id: "beach", label: "Beach", count: 15 },
-      { id: "cycling", label: "Cycling", count: 12 },
-      { id: "sauna", label: "Sauna", count: 12 },
-      { id: "night-lights", label: "Night lights", count: 12 },
-    ],
-  },
-  results: [
-    {
-      hotelId: "lakeside-motel-warefront-1",
-      name: "Lakeside Motel Warefront",
-      rating: 4.5,
-      reviewsCount: 1200,
-      description:
-        "Live a little and relax with champagne\nReimagine a divine escape in the heart of the city with Parkside Conservation, parking and a late checkout. Only include Free cancellation applies",
-      badge: "Book now and receive 15% off",
-      badgeCol: "r",
-      pricePerNight: "$130",
-    },
-    {
-      hotelId: "julia-dens-resort",
-      name: "Julia Dens Resort",
-      rating: 4.5,
-      reviewsCount: 1200,
-      description:
-        "Live a little and relax with champagne\nReimagine a divine escape in the heart of the city with Parkside Conservation, parking and a late checkout. Only include Free cancellation applies",
-      pricePerNight: "$240",
-    },
-    {
-      hotelId: "achmin-septer-hotel",
-      name: "Achmin Septer Hotel",
-      rating: 4.5,
-      reviewsCount: 1200,
-      description:
-        "Live a little and relax with champagne\nReimagine a divine escape in the heart of the city with Parkside Conservation, parking and a late checkout. Only include Free cancellation applies",
-      badge: "Receive 30% discount on extra nights",
-      badgeCol: "o",
-      pricePerNight: "$300",
-    },
-    {
-      hotelId: "regindford-hotel",
-      name: "Regindford Hotel",
-      rating: 4.5,
-      reviewsCount: 1200,
-      description:
-        "Live a little and relax with champagne\nReimagine a divine escape in the heart of the city with Parkside Conservation, parking and a late checkout. Only include Free cancellation applies",
-      pricePerNight: "$120",
-    },
-    {
-      hotelId: "shangri-open-house",
-      name: "Shangri Open House",
-      rating: 4.5,
-      reviewsCount: 1200,
-      description:
-        "Live a little and relax with champagne\nReimagine a divine escape in the heart of the city with Parkside Conservation, parking and a late checkout. Only include Free cancellation applies",
-      pricePerNight: "$145",
-    },
-    {
-      hotelId: "ocean-waves-resort",
-      name: "Ocean Waves Resort",
-      rating: 4.5,
-      reviewsCount: 1200,
-      description:
-        "Live a little and relax with champagne\nReimagine a divine escape in the heart of the city with Parkside Conservation, parking and a late checkout. Only include Free cancellation applies",
-      pricePerNight: "$310",
-    },
-    {
-      hotelId: "main-city-frontier",
-      name: "Main City frontier",
-      rating: 4.5,
-      reviewsCount: 1200,
-      description:
-        "Live a little and relax with champagne\nReimagine a divine escape in the heart of the city with Parkside Conservation, parking and a late checkout. Only include Free cancellation applies",
-      badge: "Receive 30% discount on extra nights",
-      badgeCol: "o",
-      pricePerNight: "$190",
-      oldPrice: "$200",
-    },
-    {
-      hotelId: "lakeside-motel-warefront-2",
-      name: "Lakeside Motel Warefront",
-      rating: 4.5,
-      reviewsCount: 1200,
-      description:
-        "Live a little and relax with champagne\nReimagine a divine escape in the heart of the city with Parkside Conservation, parking and a late checkout. Only include Free cancellation applies",
-      pricePerNight: "$300",
-      oldPrice: "$200",
-    },
-  ],
-};
-
-export const productDetailData = {
-  hotel: {
-    id: "lakeside-motel-warefront",
-    name: "Lakeside Motel Warefront",
-    rating: 4.5,
-    reviewsCount: 1200,
-    address: "Lorem ipsum road, Tantri\n-2322, Melbourne, Australia",
-    overviewText:
-      "Featuring free WiFi throughout the property, Lakeside Motel Waterfront offers accommodations in Lakes Entrance, 19 mi from Bairnsdale. Free private parking is available on site.<br><br>Each room at this motel is air conditioned and comes with a flat-screen TV. You will find a kettle, toaster and a microwave in the room. Each room is fitted with a private bathroom. Guests have access to barbecue facilities and a lovely large lawn area. Metung is 6.8 mi from Lakeside Motel Waterfront, while Paynesville is 14 mi from the property.<br><br>Couples in particular like the location – they rated it 9.2 for a two-person trip.",
-  },
-  topFacilities: [
-    { id: "wifi", label: "Free wifi" },
-    { id: "ac", label: "Air Conditioning" },
-    { id: "parking", label: "Parking available" },
-    { id: "business", label: "Business Services" },
-    { id: "pool", label: "Swimming pool" },
-    { id: "top-rated", label: "Top rated in area" },
-  ],
-  exploreArea: [
-    { name: "Hotel Pennsylvania", distance: "2 min drive" },
-    { name: "Travis Bakery store house", distance: "10 min drive" },
-    { name: "Olivia Johnson Garden", distance: "15 min drive" },
-    { name: "Norman Opera Circus", distance: "18 min drive" },
-    { name: "Rockdesert hotel", distance: "32 min drive" },
-  ],
-  availability: {
-    checkin: "Mar 18, 2022",
-    checkout: "March 20, 2022",
-    guests: "2 adult, 1 room",
-  },
-  promoCard: {
-    title: "20% off<br>Use Promotional<br>Coupon Code:<br><span>Orlando</span>",
-  },
-  rooms: [
-    {
-      id: "standard-twin-multiple-beds",
-      name: "Standard twin ben, Multiple beds",
-      price: 300,
-      currency: "$",
-      sleeps: 3,
-      beds: "1 double bed and 1 twin bed",
-    },
-    {
-      id: "standard-twin-queen-bed",
-      name: "Standard twin ben, 1 Queen bed",
-      price: 300,
-      currency: "$",
-      sleeps: 3,
-      beds: "1 double bed and 1 twin bed",
-    },
-  ],
-};
-
-export const checkoutData = {
-  room: {
-    roomLabel: "Room 1",
-    summary: "2 adults, 1 double bed and 1 twin bed, Non-smoking",
-  },
-  summaryCard: {
-    hotelId: "lakeside-motel-warefront",
-    hotelName: "Lakeside Motel Warefront",
-    rating: 4.5,
-    reviewsCount: 1200,
-    policy: "Non refundable",
-    checkIn: "Sunday, March 18, 2022",
-    checkOut: "Tuesday, March 20, 2022",
-    stayNights: 2,
-  },
-  priceDetails: {
-    items: [
-      { description: "1 room X 2 nights", amount: 120.32 },
-      { description: "Tax and service fees", amount: 8.32 },
-    ],
-    total: 130,
-    currency: "$",
-  },
-  policyItems: [
-    "This rate is non-refundable. If you change or cancel your booking you will not get a refund or credit to use for a future stay.",
-    "Stay extensions will require a new reservation.",
-    "Front desk staff will greet guests on arrival",
-    "No refunds will be issued for late check-in or early check-out.",
-  ],
-};
-
-export const myTripsData = {
-  trips: [
-    {
-      bookingId: "b1",
-      hotelId: "lakeside-motel-warefront",
-      hotelName: "Lakeside Motel Warefront",
-      rating: 4.5,
-      reviewsCount: 1200,
-      policy: "Non refundable",
-      checkIn: "Sunday, March 18, 2022",
-      checkOut: "Tuesday, March 20, 2022",
-      stayNights: 2,
-      rooms: 1,
-      oldPrice: 150,
-      price: 130,
-      currency: "$",
-    },
-  ],
-  suggestedDestinations: [
-    {
-      id: "australia",
-      nom: "Australia",
-      imatgeUrl: "australia.jpg",
-      propietatsCount: 2246,
-    },
-    {
-      id: "japan",
-      nom: "Japan",
-      imatgeUrl: "japon.jpg",
-      propietatsCount: 1278,
-    },
-    {
-      id: "new-zealand",
-      nom: "New Zealand",
-      imatgeUrl: "nueva-zelanda.jpg",
-      propietatsCount: 480,
-    },
-    {
-      id: "greece",
-      nom: "Greece",
-      imatgeUrl: "grecia.jpg",
-      propietatsCount: 320,
-    },
-  ],
-};
+// export const myTripsData = {
+//   trips: [
+//     {
+//       bookingId: "b1",
+//       hotelId: "lakeside-motel-warefront",
+//       hotelName: "Lakeside Motel Warefront",
+//       rating: 4.5,
+//       reviewsCount: 1200,
+//       policy: "Non refundable",
+//       checkIn: "Sunday, March 18, 2022",
+//       checkOut: "Tuesday, March 20, 2022",
+//       stayNights: 2,
+//       rooms: 1,
+//       oldPrice: 150,
+//       price: 130,
+//       currency: "$",
+//     },
+//   ],
+//   suggestedDestinations: [  
+//     {
+//       id: "australia",
+//       nom: "Australia",
+//       imatgeUrl: "australia.jpg",
+//       propietatsCount: 2246,
+//     },
+//     {
+//       id: "japan",
+//       nom: "Japan",
+//       imatgeUrl: "japon.jpg",
+//       propietatsCount: 1278,
+//     },
+//     {
+//       id: "new-zealand",
+//       nom: "New Zealand",
+//       imatgeUrl: "nueva-zelanda.jpg",
+//       propietatsCount: 480,
+//     },
+//     {
+//       id: "greece",
+//       nom: "Greece",
+//       imatgeUrl: "grecia.jpg",
+//       propietatsCount: 320,
+//     },
+//   ],
+// };
